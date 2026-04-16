1c0559826033e3d822be4cb76d14e11b67999ed9
VFX <vfxshambhoo@gmail.com>
2026-04-16 09:17:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1751399240-6c00c",
+  "appId": "1:1020595500841:web:992662b8a570e8e160a938",
+  "apiKey": "AIzaSyBKm-4emej762upZV3SciqvgtLaypmO4VA",
+  "authDomain": "studio-1751399240-6c00c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1020595500841"
+};
